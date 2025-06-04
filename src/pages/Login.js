e3ea4a6798efcdf32944e845f145a2c836d5b203
Bianca Hurjui <b.hurjui@yahoo.com>
2025-06-04 17:15:25 +0300

    Fix login URL for deploy

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ function LoginPage() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:3001/login', {
+      const response = await fetch('https://pet-backend2.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
